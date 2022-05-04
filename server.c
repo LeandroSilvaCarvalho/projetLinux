1d373d7995e03b5bde827459e1dfeb741d19f730
Abdenour didi <abdenour.didi@student.vinci.be>
2022-05-04 15:48:13 +0200

    init server socket

--- server.c
+++ server.c
@@ -1,7 +1,34 @@
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 
+#include "messages.h"
+#include "utils_v1.h"
+#include "config.h"
+
+int initSocketServer(int port)
+{
+  int sockfd = ssocket();
+  sbind(port, sockfd);
+  slisten(sockfd, BACKLOG);
+  return sockfd;
+}
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    if(argc<1){
+        perror("Missing arguments\n");
+        exit(0);
+    }
+    int port = atoi(argv[1]);
+    int sockfd = initSocketServer(port);
+    printf("Le serveur tourne sur le port : %i \n", port);
+
     return 0;
 }
