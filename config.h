0c253a5fa71763fc424f198070c2b358e680cc25
LeandroSilvaCarvalho <leandrosdc.pro@gmail.com>
2022-05-04 14:14:10 +0200

    config file

---
+++ config.h
@@ -0,0 +1,11 @@
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+#define SERVER_PORT 	9501
+#define SERVER_IP		"127.0.0.1"  /* localhost */
+
+#define SHM_KEY 248
+#define SEM_KEY 369
+#define PERM 0666
+
+#endif
