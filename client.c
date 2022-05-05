20dc57520ece2aabb607a4157a159dba17ad7feb
Abdenour didi <abdenour.didi@student.vinci.be>
2022-05-05 11:37:56 +0200

    init server

--- client.c
+++ client.c
@@ -1,5 +1,51 @@
-int main(int argc, char const *argv[])
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include "messages.h"
+#include "utils_v1.h"
+#include "config.h"
+
+int initSocketClient(char ServerIP[16], int Serverport)
 {
-    /* code */
+    int sockfd = ssocket();
+    sconnect(ServerIP, Serverport, sockfd);
+    return sockfd;
+
+}
+
+int main(int argc, char *argv[])
+{
+    /*if(argc<4){
+        perror("Missing argument");
+        exit(0);
+    }*/
+
+    int port = atoi(argv[2]);
+
+    structMessage msg;
+    int ret = sread(0, msg.messageText, MAX_TEXT);
+    msg.messageText[ret-1] = '\0';
+    msg.code = INSCRIPTION_REQUEST;
+
+    int sockfd = initSocketClient(argv[1], port);
+    swrite(sockfd, &msg, sizeof(msg));
+    sread(sockfd, &msg, sizeof(msg));
+    if (msg.code == INSCRIPTION_OK)
+    {
+    printf("Réponse du serveur : Inscription acceptée\n");
+  }
+  else if (msg.code == INSCRIPTION_KO)
+  {
+    printf("Réponse du serveur : Inscription refusée\n");
+  }
+
+  sclose(sockfd);
     return 0;
 }
--- server.c
+++ server.c
@@ -12,6 +12,8 @@
 #include "utils_v1.h"
 #include "config.h"
 
+
+
 int initSocketServer(int port)
 {
   int sockfd = ssocket();
@@ -22,13 +24,45 @@ int initSocketServer(int port)
 
 int main(int argc, char const *argv[])
 {
+    structMessage msg;
     if(argc<1){
         perror("Missing arguments\n");
         exit(0);
     }
+    
+    struct pollfd fds[1024];
+    bool fds_invalid[1024];
+    int nbSockfd = 0;
+
     int port = atoi(argv[1]);
     int sockfd = initSocketServer(port);
     printf("Le serveur tourne sur le port : %i \n", port);
+    
+    fds[nbSockfd].fd = sockfd;
+    fds[nbSockfd].events = POLLIN;
+    fds_invalid[nbSockfd] = false;
+    nbSockfd++;
+    int newSockfd;
 
+
+    while(1){
+      spoll(fds, nbSockfd, 5000);
+
+      if(fds[0].revents & POLLIN & !fds_invalid[0]){
+        newSockfd = saccept(sockfd);
+        fds[nbSockfd].fd = newSockfd;
+		    fds[nbSockfd].events = POLLIN;
+		    fds_invalid[nbSockfd] = false;
+        nbSockfd++;
+        sread(newSockfd, &msg, sizeof(msg));
+        printf("bonjour %s\n", msg.messageText);
+        printf("aurevoir %s\n", msg.messageText);
+        msg.code = INSCRIPTION_OK;
+      }
+      nwrite(newSockfd, &msg, sizeof(msg));
+    }
+
+    sclose(sockfd);
+    
     return 0;
 }
