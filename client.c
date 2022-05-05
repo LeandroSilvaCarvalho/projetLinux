a3eca88bea5d6c786aefe207a75e33b18188eb2a
LeandroSilvaCarvalho <leandrosdc.pro@gmail.com>
2022-05-05 10:54:49 +0200

    client.c

--- client.c
+++ client.c
@@ -1,5 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <fcntl.h>
+
+#include "utils_v1.h"
+
+int initSocketClient(char ServerIP[16], int Serverport)
+{
+	int sockfd = ssocket();
+	sconnect(ServerIP, Serverport, sockfd);
+	return sockfd;
+}
+
 int main(int argc, char const *argv[])
 {
-    /* code */
     return 0;
 }
