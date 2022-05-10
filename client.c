61647f949236a91c4bda632bc8d0232f969be235
LeandroSilvaCarvalho <leandrosdc.pro@gmail.com>
2022-05-10 12:45:49 +0200

    client.c

--- client.c
+++ client.c
@@ -10,6 +10,7 @@
 
 #include "utils_v1.h"
 #include "transfer.h"
+#include "messages.h"
 
 #define CHOICE_SIZE 50
 
@@ -27,18 +28,56 @@ void propositions()
     printf("To quit: q\n");
 }
 
-void child_timer(int delay, StructTransfer pipe)
+void child_timer(void *d, void *p)
 {
+    int *delay = d;
+    int *pipe = p;
+    sclose(pipe[0]);
+
     while (1)
     {
-
-        sleep(delay);
+        sleep(*delay);
+        StructTransfer transfer;
+        transfer.receiver = -1;
+        swrite(pipe[1], &transfer, sizeof(transfer));
     }
+    exit(EXIT_SUCCESS);
 }
 
-void child_transfer_(StructTransfer pipe)
+void child_recurrent_transfer(void *p, void *a, void *po)
 {
+    int *pipe = p;
+    char *adr = a;
+    int *port = po;
 
+    sclose(pipe[1]);
+    StructTransfer transfers[100];
+    int sizeTransfers = 0;
+
+    int nbRd;
+    StructTransfer transfer;
+    while ((nbRd = sread(pipe[0], &transfer, sizeof(transfer))) != 0)
+    {
+        if (transfer.receiver == -1)
+        {
+            StructMessage message;
+            message.sizeTransfers = sizeTransfers;
+            int sockfd = initSocketClient(adr, *port);
+            for (int i = 0; i < sizeTransfers; i++)
+            {
+                message.transfers[i] = transfers[i];
+            }
+            swrite(sockfd, &message, sizeof(message));
+            sclose(sockfd);
+        }
+        else
+        {
+            transfers[sizeTransfers] = transfer;
+            sizeTransfers++;
+        }
+    }
+    sclose(pipe[0]);
+    exit(EXIT_SUCCESS);
 }
 
 int main(int argc, char const *argv[])
@@ -63,14 +102,14 @@ int main(int argc, char const *argv[])
         StructTransfer transfer;
         transfer.sender = num;
 
-        StructTransfer pipe[2];
+        int pipe[2];
 
         spipe(pipe);
 
-        fork_and_run2(child_timer, delay, pipe);
-        fork_and_run1(child_transfer_, pipe);
+        int timerId = fork_and_run2(child_timer, &delay, &pipe);
+        int recurrentTrasferId = fork_and_run3(child_recurrent_transfer, &pipe, &adr, &port);
 
-
+        sclose(pipe[0]);
         propositions();
 
         char choice[CHOICE_SIZE];
@@ -109,10 +148,25 @@ int main(int argc, char const *argv[])
                     int sockfd = initSocketClient(adr, port);
                     transfer.receiver = atoi(dividedChoise[1]);
                     transfer.amount = atoi(dividedChoise[2]);
-                    swrite(sockfd, &transfer, sizeof(transfer));
+
+                    StructMessage message;
+                    message.transfers[0] = transfer;
+                    message.sizeTransfers = 1;
+                    swrite(sockfd, &message, sizeof(message));
 
                     // Message a ajouter
-
+                    sread(sockfd, &message, sizeof(message));
+                    if (message.code == INSCRIPTION_OK)
+                    {
+                        printf("The transfer has been done\n");
+                        int newSold = atoi(message.messageText);
+                        printf("The new sold of your account is:%d\n", newSold);
+                    }
+                    else
+                    {
+                        printf("An error occurred\n");
+                        printf("The transfer hasn't been done\n");
+                    }
                     sclose(sockfd);
                 }
                 else if (choice[0] == '*')
@@ -120,6 +174,9 @@ int main(int argc, char const *argv[])
                     printf("type:%s|\n", dividedChoise[0]);
                     printf("compte:%s|\n", dividedChoise[1]);
                     printf("montant:%s|\n", dividedChoise[2]);
+                    transfer.receiver = atoi(dividedChoise[1]);
+                    transfer.amount = atoi(dividedChoise[2]); 
+                    swrite(pipe[1], &transfer, sizeof(transfer));
                 }
             }
 
@@ -127,8 +184,10 @@ int main(int argc, char const *argv[])
             sread(0, choice, CHOICE_SIZE);
             choice[strlen(choice) - 1] = '\0';
         }
+        sclose(timerId);
+        sclose(recurrentTrasferId);
+        sclose(pipe[1]);
     }
-    sclose(pipe);
 
     return 0;
 }
